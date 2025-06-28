6a415e2c10b53ca87fc9e0191d969d6925dcc09e
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-06-28 12:54:32 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of components
+    """
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt','r',encoding='utf-8') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                ##ignore empty lines and -e .
+                if requirement and not requirement.startswith("#") and requirement!='-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    return requirement_list               
+
+print(get_requirements)
+
+setup(
+    name="Network Security",
+    version="0.0.1",
+    author="Yashvardhan Singh",
+    author_email="yashvardhansingh9532@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
