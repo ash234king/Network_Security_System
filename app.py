e7f8f866616d7794b74829c67d83cca01cbe13bc
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-07-01 22:45:16 +0530

    updated

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         #df['predicted_column'].replace(-1, 0)
         #return df.to_json()
         df.to_csv('prediction_output/output.csv')
-        table_html = df.to_html(classes='table table-striped')
+        table_html = df.to_html(classes='data-table')
         #print(table_html)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
         
